72dce173040734e8a357ba8a0446c28e5608996a
Gabriel Mejia <endermejia032@gmail.com>
2022-06-03 16:30:37 +0200

    FEATURE: responsive sidenav

--- src/app/blog/pages/home/home.component.ts
+++ src/app/blog/pages/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDrawer, MatDrawerMode, MatSidenav} from '@angular/material/sidenav';
+
+// Sidenav mode constants
+export const SIDENAV_MODE_SIDE: MatDrawerMode = 'side';
+export const SIDENAV_MODE_OVER: MatDrawerMode = 'over';
+export const SIDENAV_MODE_PUSH: MatDrawerMode = 'push';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  public closeSidenav(drawer: MatSidenav): void {
+    if (this.isMobile) {
+      drawer.close().then(() => console.log('Closed'));
+    }
+  }
+
+  public get isMobile(): boolean {
+    return window.innerWidth < 768;
+  }
+
+  public get sidenavMode(): MatDrawerMode {
+    return this.isMobile
+      ? SIDENAV_MODE_PUSH
+      : SIDENAV_MODE_SIDE;
   }
 
 }
