f8425d21654f84992144964d62db9b329c8ca28a
Gabriel Mejia <endermejia032@gmail.com>
2022-06-23 20:49:26 +0200

    FEATURE: contact model

---
+++ src/app/blog/models/contact.model.ts
@@ -0,0 +1,13 @@
+export interface ContactModel {
+  title: string;
+  firstParagraph: string;
+  emailCECB: string;
+  secondParagraph: string;
+  formTitle: string;
+  formspree: string;
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+  send: string;
+}
--- src/app/blog/pages/contact/contact.component.ts
+++ src/app/blog/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ContactModel} from '../../models/contact.model';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  public contact: ContactModel = {
+    title: '📧 Contáctanos',
+    firstParagraph: 'Puedes contactar con nosotros enviando un email a la siguiente dirección:',
+    emailCECB: 'clubescaladacostablanca@gmail.com',
+    secondParagraph: 'O bien, puedes rellenar el siguiente formulario de contacto:',
+    formTitle: 'Formulario de contacto',
+    formspree: 'https://formspree.io/f/moqrrrer',
+    name: 'Nombre',
+    email: 'Email',
+    phone: 'Teléfono',
+    message: 'Mensaje',
+    send: 'Enviar'
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
