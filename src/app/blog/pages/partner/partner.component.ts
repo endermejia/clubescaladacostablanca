9df539b1746056d659efd9e9a9b6a891f91854dd
Gabriel Mejia <endermejia032@gmail.com>
2022-07-22 18:49:01 +0200

    FEATURE: partnerForm

--- src/app/blog/pages/partner/partner.component.ts
+++ src/app/blog/pages/partner/partner.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-partner',
@@ -7,10 +8,72 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PartnerComponent implements OnInit {
 
-  constructor() {
+  partnerForm: FormGroup | undefined;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
   }
 
   ngOnInit(): void {
+    this.partnerForm = this.fb.group({
+      name: [
+        '',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
+      ],
+      lastName: [
+        '',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
+      ],
+      secondLastName: [
+        '',
+        [Validators.minLength(3), Validators.maxLength(50)]
+      ],
+      documentNumber: [
+        '',
+        [Validators.required, Validators.minLength(8), Validators.maxLength(8)]
+      ],
+      phone: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]+$')]
+      ],
+      email: [
+        '',
+        [Validators.required, Validators.email, Validators.maxLength(50)]
+      ],
+      gender: [
+        '',
+        [Validators.required]
+      ],
+      birthDate: [
+        '',
+        [Validators.required]
+      ],
+      address: [
+        '',
+        [Validators.required]
+      ],
+      zipCode: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]+$')]
+      ],
+      city: [
+        '',
+        [Validators.required]
+      ],
+      country: [
+        '',
+        [Validators.required]
+      ],
+      whatsapp: [
+        '',
+        [Validators.required]
+      ],
+      privacyPolicy: [
+        '',
+        [Validators.required]
+      ]
+    });
   }
 
 }
